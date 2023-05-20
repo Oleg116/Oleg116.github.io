29fd051773c2d40a737424a9012f6d4412c5cc73
Oleg116 <3006.azza@gmail.com>
2023-05-20 12:22:05 +0300

    Add footer navbar

--- freysoft/src/App.js
+++ freysoft/src/App.js
@@ -1,21 +1,21 @@
 import React from 'react'
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom'
 import Box from '@mui/material/Box'
 import LabelBottomNavigation from './components/LabelBottomNavigation'
 
-function App () {
+function App() {
   return (
     <div className="App">
-    <BrowserRouter>
-    <Routes>
-        <Route index element={<Box>2</Box>} />
-        <Route path="blogs" element={<Box>3</Box>} />
-        <Route path="*" element={<Box>404</Box>} />
-    </Routes>
-  </BrowserRouter>
-  <LabelBottomNavigation />
-  </div>
+      <BrowserRouter>
+        <Routes>
+          <Route index path='/home' element={<Box>2</Box>} />
+          <Route path="Expenses" element={<Box>3</Box>} />
+          <Route path="*" element={<Box>404</Box>} />
+        </Routes>
+        <LabelBottomNavigation />
+      </BrowserRouter>
+    </div>
   )
 }
 
--- freysoft/src/components/LabelBottomNavigation.tsx
+++ freysoft/src/components/LabelBottomNavigation.tsx
@@ -1,41 +1,71 @@
 import * as React from 'react';
+
+import { Link } from "react-router-dom";
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import Box from '@mui/material/Box';
 import FolderIcon from '@mui/icons-material/Folder';
-import RestoreIcon from '@mui/icons-material/Restore';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
+import CreditCardRoundedIcon from '@mui/icons-material/CreditCardRounded';
+import CalendarTodayOutlinedIcon from '@mui/icons-material/CalendarTodayOutlined';
 
+const styles = {
+  round: {
+    width: '50px',
+    height: '50px',
+    borderRadius: '50%',
+    backgroundColor: '#FF643B',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+}
 export default function LabelBottomNavigation() {
-  const [value, setValue] = React.useState('recents');
+  const [value, setValue] = React.useState('home');
 
-  const handleChange = (event: any, newValue: React.SetStateAction<string>) => {
+  const handleChange = (event: React.MouseEventHandler<HTMLAnchorElement>, newValue: React.SetStateAction<string>) => {
     setValue(newValue);
   };
+  console.log(value);
 
   return (
-    <BottomNavigation sx={{ width: 500 }} value={value} onChange={handleChange}>
-      <BottomNavigationAction
-        label="Recents"
-        value="recents"
-        icon={<RestoreIcon />}
-      />
-      <BottomNavigationAction
-        label="Favorites"
-        value="favorites"
-        icon={<FavoriteIcon />}
-      />
-      <BottomNavigationAction
-        label="Nearby"
-        value="nearby"
-        icon={<LocationOnIcon />}
-      />
-      <BottomNavigationAction
-        label="Nearby"
-        value="nearby"
-        icon={<LocationOnIcon />}
-      />
-      <BottomNavigationAction label="Folder" value="folder" icon={<FolderIcon />} />
+    <BottomNavigation sx={{ width: 500, display: 'flex', justifyContent: 'center', alignItems: 'center' }} value={value}>
+      <Link to="/home" onClick={() => setValue('home')}>
+        <BottomNavigationAction
+          label="Home"
+          value="home"
+          icon={<HomeRoundedIcon sx={{ color: value === 'home' ? '#FF643B' : '#A3A3A3' }} />}
+        />
+      </Link>
+      <Link to="/expenses" onClick={() => setValue('expenses')}>
+        <BottomNavigationAction
+          label="Favorites"
+          value="favorites"
+          icon={<CreditCardRoundedIcon sx={{ color: value === 'expenses' ? '#FF643B' : '#A3A3A3' }} />}
+        />
+      </Link>
+      <Link to="/add" onClick={() => setValue('add')}>
+        <BottomNavigationAction
+          label="add"
+          value="Add"
+          icon={<Box sx={styles.round}><AddOutlinedIcon sx={{ color: '#fff' }} /></Box>}
+        />
+      </Link>
+      <Link to="/calendar" onClick={() => setValue('calendar')}>
+        <BottomNavigationAction
+          label="Calendar"
+          value="calendar"
+          icon={<CalendarTodayOutlinedIcon sx={{ color: value === 'calendar' ? '#FF643B' : '#A3A3A3' }} />}
+        />
+      </Link>
+      <Link to="/setting" onClick={() => setValue('setting')}>
+        <BottomNavigationAction
+          label="Setting"
+          value="setting"
+          icon={<SettingsOutlinedIcon sx={{ color: value === 'setting' ? '#FF643B' : '#A3A3A3' }} />} />
+      </Link>
     </BottomNavigation>
-  );
-}
+  )
+};
